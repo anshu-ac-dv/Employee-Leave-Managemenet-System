d41fd69bc4bec3bc3e128c11bada2ddbc7afe0e5
Anshu Kumar <111151635+anshukr963@users.noreply.github.com>
2023-02-23 08:52:27 +0530

    Add files via upload

--- Header.php
+++ Header.php
@@ -6,14 +6,17 @@
   <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-    <nav class="navbar shadow navbar-expand-sm navbar-light bg-lg p-3 fixed">
-      <a href="#" class="navbar-brand ms-3">Welcome</a>
+    <nav class="navbar shadow navbar-expand-sm navbar-light bg-lg p-3">
+      <a href="#" class="navbar-brand ms-3">Casinal</a>
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item active"><a href="Dashboard.php" class="nav-link">Dashboard</a></li>
-        <li class="nav-item"><a href="UpdateProfile.php" class="nav-link">Update-Profile</a></li>
-        <li class="nav-item"><a href="Attendance.php" class="nav-link">Attendance</a></li>
-        <li class="nav-item"><a href="Leave.php" class="nav-link">Apply-For-Leave</a></li>
-        <a href="Logout.php"><button type="button" class="btn btn-info">Logout</button></a>
+        <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="Courses.php" class="nav-link">Courses</a></li>
+        <li class="nav-item"><a href="Tutorial.php" class="nav-link">Tutorial</a></li>
+        <li class="nav-item"><a href="Blog.php" class="nav-link">Blog</a></li>
+        <li class="nav-item"><a href="Contact.php" class="nav-link">Contact</a></li>
+        <li class="nav-item"><a href="About.php" class="nav-link">About</a></li>
+        <button type="button" class="btn btn-info ms-3 mx-3">Sign Up</button>
+        <button type="button" class="btn btn-info">Login</button>
       </ul>
       <hr>
     </nav>
